ee77f8e32ef250e0ae7cdfe3b074a23f3716a00d
Andrew Glines <aglines@gmail.com>
2018-03-28 21:34:16 +0100

    generating text from rand numbers, outputs to root URL

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+from random import randint
 
 # Initialize application
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Tweet a compliment sandwich to the biggest asshole of the modern age - coming soon"
+    tweetText = assembleText()
+    return render_template('index.html', tweetText=tweetText)
+
+def assembleText():
+    randIntro = randint(0,2)
+    randMid = randint(0,2)
+    randOutro = randint(0,1)
+
+    listIntro = ['intro1', 'intro2', 'intro3']
+    listMid = ['mid1', 'mid2', 'mid3']
+    listOutro = ['out1', 'out2']
+
+    sandwich = listIntro[randIntro] + ' ' + listMid[randMid] + ' ' + listOutro[randOutro]
+
+    return sandwich
