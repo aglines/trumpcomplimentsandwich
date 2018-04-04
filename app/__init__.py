4c42f9bdf6f417c611f6dec0e2af8c80e69977c7
Andrew Glines <aglines@gmail.com>
2018-04-04 10:56:55 +0100

    update images, adjust tweet text, clean up code

--- app/__init__.py
+++ app/__init__.py
@@ -21,15 +21,15 @@ def tweet():
 def assembleIntro():
     randIntro = randint(0,19)
     textIntro = [
-    'MAGA TRUMP!','TRUMP 2020!','TRUMP America Great!','Keep America TRUMP!','TRUMP steaks great!','TRUMP Tower lu$$xury!','TRUMP #1!','So great TRUMP!','Handsomest Prez TRUMP!','TRUMP $$$!','$$ TRUMP so rich!','TRUMP the BEST!','TRUMP strong POWER!','TRUMPs handsome genes!','Clear WINNER!','TRUMP knows!','World\'s. Best. TRUMP!','My hero TRUMP!','Rename it TRUMPDAY!','Winning TRUMP!'
+    'MAGA TRUMP!','TRUMP 2020!','TRUMP America Great!','Keep America TRUMP!','TRUMP Steaks $$$!','TRUMP Tower $$$!','TRUMP Luxury!','Go TRUMP!','Handsomest Prez TRUMP!','TRUMP $$$!','TRUMP so $$rich!','TRUMP the BEST!','TRUMP strong POWER!','TRUMPs handsome genes!','Clear WINNER Trump!','TRUMP Made America Great!','Best leader TRUMP!','My hero TRUMP!','Rename it TRUMPDAY!','Winning TRUMP!'
     ]
     return textIntro[randIntro]
 
 def assembleMid():
     randMid = randint(0,29)
     textMid = [
-    'jail time for you.','you\'re gonna die in prison.','your father never loved you.','your dad never cared about you.','DC lawyers all know you\'re poison.','you can\'t trust your men anymore.','impeached before long.','no lawyer is dumb enough to defend you now.','everyone knows you\'re poor.','you feel old and you look old.','we can all see you\'re horribly bald.','walls are closing in around you.','lawyers are too smart for you.','lawyers can smell your lies.','every old buddy is ratting on you this very moment.','your sons only want your money.','he only wants your money.','white house staff fleeing your sinking ship.','don jr dancing on your grave.','ivanka and jared are ratting on you.','lawyers are all jumping your sinking ship.','hope hicks is ratting on you now too.','ivanka talked to mueller can\'t trust her.','don jr talk to mueller can\'t trust him.','jared talked to mueller.','can\'t trust them better fire them.','can\'t even trust your bodyguards now.','they\'re coming for you.','they\'re going to get you in your sleep.','don\'t trust your bodyguards.']
-
+    'jail time for you','that wall\'s never going up','your father never loved you','your dad never cared about you','lawyers all know you\'re poison','you can\'t trust anyone now','impeached before long','no lawyer is dumb enough to defend you','everyone knows you\'re not that rich','you feel old and you look old','we can see you\'re bald','walls are closing in around you.','lawyers can smell your lies','every old business buddy singin like a canary','every friend has been ratting on you for months','your sons only want your money','they only wants your money','white house staff fleeing your sinking ship','don jr dancing on your grave','ivanka and jared are ratting on you','lawyers are all jumping your sinking ship','hope hicks ratting on you now too','ivanka talked to mueller can\'t trust her','don jr squealed can\'t trust him','jared squealed can\'t trust him','can\'t trust anyone fire them all','can\'t even trust your bodyguards now','they\'re coming for you','they\'re gonna get you','even your best allies are incompetent'
+    ]
     return textMid[randMid]
 
 def assembleOutro():
@@ -38,8 +38,6 @@ def assembleOutro():
     'TRUMP family 4ever!','TRUMP give em hell!','TrUmP!','TRUMP Richest Prez!','TRUMP Best Potus ever!','Trump Power!','TRUMP Muscle!','TRUMP America!','TRUMP America GREAT!','TRUMP Tower GOLD$$!','TRUMP $$!!','$$ TRUMP so rich!','TRUMP Tower $trong!','TRUMP America Great Again!','MAGA bitches!!','MAGA 2024!','MAGA Biggest fans!','TRUMP MAGA biggest ever!','MAGA 2020!','2020 TRUMP!'
     ]
     return textOutro[randOutro]
-
-
 
 def assembleURL(tweetText):
     tweetText = re.sub(' ','%20',tweetText)
